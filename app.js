f2714bd228318dd9a3fe23fa564a72863a22a758
Rohit <iamrohit.panjwani@gmail.com>
2020-01-09 13:40:32 +0530

    rewrote for tcp connection

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
-const express = require('express');
-const fs = require('fs');
+const server = require("net").createServer();
 
-const app = express();
-
-app.use((req, res) => {
-    let body = [];
-    body = [];
-    req.on('data', (data) => {
-        body.push(data);
-    });
-    req.on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body);
-        fs.readFile('data.txt', 'utf8', (err, data) => {
-            if (err) {
-                console.log(err);
+server.on("connection", socket => {
+    socket.setEncoding("hex");
+    const client = `${socket.remoteAddress}:${socket.remotePort}`;
+    socket.setKeepAlive(true, 50000);
+    socket.on("data", data => {
+        const parsed__ = parser(data);
+        if (
+            Array.isArray(parsed__) &&
+            parsed__.length > 0 &&
+            typeof parsed__[0] === "object" &&
+            parsed__[0].case
+        ) {
+            const imei =
+                parsed__[0].case === "01" ?
+                helpers.imei_manager.set(
+                    parsed__[0].imei,
+                    socket.remoteAddress,
+                    socket.remotePort
+                ) :
+                helpers.imei_manager.get(
+                    socket.remoteAddress,
+                    socket.remotePort
+                );
+            if (imei) {
+                helpers.data_middleware(parsed__.map(k => ({
+                    ...k,
+                    imei,
+                    socket: client
+                })));
             }
-            else {
-                data = data + '\n\n---------------------------------\n\n' + body;
-                fs.writeFile('data.txt', data, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        console.log('\n\n---------------------------------\n\n');
-                    }
-                    res.send();
-                })
-            }
-            res.send();
-        })
+            parsed__.filter(k => k.output).forEach((k) => {
+                socket.write(
+                    Buffer.from(
+                        k.output.match(/.{2}/g).map(i => parseInt(i, 16))
+                    ));
+            });
+        } else helpers.send_invalid_data_to_api(data);
     });
-})
+    socket.on("error", err => {
+        console.error({ event: "error", err: err.message, client });
+        socket.end();
+    });
+    socket.on("close", () => {
+        console.log({ event: "close", client });
+        helpers.imei_manager.delete(client);
+    });
+    socket.setTimeout(1000 * 60 * 30, () => {
+        console.log('Socket Timeout', client);
+        helpers.imei_manager.delete(client);
+        socket.end();
+    });
+    socket.on("end", (hadError) => {
+        helpers.imei_manager.delete(client);
+        console.log({ event: "end", client, hadError });
+    });
+});
 
-app.listen(8000, () => {
-    console.log('listening');
+server.listen(8000, () => {
+    console.log({
+        event: "CONCOX_TCP_SERVER STARTED",
+        PORT: 8000
+    });
 });
